bbc275290a6ff90da1c6ca131182bbb91e1e74be
guilhermeSF22 <162653617+guilhermeSF22@users.noreply.github.com>
2025-02-03 10:32:50 -0400

    Add files via upload

---
+++ heyder/Triangulo.java
@@ -0,0 +1,34 @@
+package Atividade004.AtividadeAvaliativa004;
+
+public class Triangulo extends Figura {
+    private double base;
+    private double altura;
+
+    public Triangulo(String cor, double base, double altura) {
+        super("triângulo", cor);
+        this.base = base;
+        this.altura = altura;
+    }
+
+    @Override
+    public double calcularArea() {
+        return (base * altura) / 2;
+    }
+
+    // Getters e Setters específicos
+    public double getBase() {
+        return base;
+    }
+
+    public void setBase(double base) {
+        this.base = base;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+}
